e4d978d24cb948e0eb8a08e8204c924084a835b7
Joe D <j.dimartino@live.com>
2020-06-17 19:44:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function sayHiToGrandma(string) {
   const key = "I love you, Grandma." 
   if (string === whisper(string)) {
     return "I can't hear you!"
-  } else if ( string === shout(string)) {
+  } else if (string === shout(string)) {
     return "YES INDEED!"
   } else if (string === key) {
     return "I love you, too."
